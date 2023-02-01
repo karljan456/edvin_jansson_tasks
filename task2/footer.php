171f598e8c0ebf2daeef237f9b294a9970bb98df
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-02-01 12:31:23 +0200

    2 In-class Task 01.02.2023 (index.php)

---
+++ task2/footer.php
@@ -0,0 +1,96 @@
+
+            <!--####################### FOOTER BEGINS HERE########################-->
+            <div class="footer-wrapper">
+                <!-- FOOTER BEGINS HERE-->
+                <footer>
+
+                    <div class="container text-center mw-100">
+
+                        <div id="trophies">
+                            <div class="row footer-row-1">
+                                <div class="col-md-2 col-sm-4 col-xs-4 trophy-box  ">
+                                    <img class="img-fluid" src="league_title.svg" alt="league_title">
+                                    <div class="titles">19</div>
+                                </div>
+                                <div class=" col-md-2 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid " src="european_cup.svg" alt="european_cup">
+                                    <div class="titles">6</div>
+                                </div>
+                                <div class=" col-md-2 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid" src="fa_cup.svg" alt="fa_cup">
+                                    <div class="titles">8</div>
+                                </div>
+                                <div class=" col-md-2 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid" src="uefa_cup.svg" alt="uefa_cup">
+                                    <div class="titles">3</div>
+                                </div>
+                                <div class="col-md-2 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid" src="league_cup.svg" alt="league_cup">
+                                    <div class="titles">9</div>
+                                </div>
+                                <div class="col-md-1 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid" src="uefa_super_cup.svg" alt="uefa_super_cup">
+                                    <div class="titles">4</div>
+                                </div>
+                                <div class="col-md-1 col-sm-4 col-xs-4 trophy-box">
+                                    <img class="img-fluid" src="fifa_club_world_cup.svg"
+                                        alt="fifa_club_world_cup">
+                                    <div class="titles">1</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row footer-row-2 ">
+                            <div class="col col-md-12">
+                                <span class="social-icon">
+                                    <a href="https://www.facebook.com/LiverpoolFC/">
+                                        <img src="facebook.svg" width="20" alt="facebook"></a>
+
+                                </span>
+                                <span class="social-icon">
+                                    <a href="https://www.instagram.com/liverpoolfc/?hl=en"><img
+                                            src="instagram.svg" width="20" alt="instagram"></a>
+
+                                </span>
+                                <span class="social-icon">
+                                    <a
+                                        href="https://twitter.com/LFC?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img
+                                            src="twitter.svg" width="20" alt="twitter"></a>
+
+                                </span>
+                                <span class="social-icon">
+                                    <a href="https://uk.linkedin.com/company/liverpool-football-club"><img
+                                            src="linkedin.svg" width="20" alt="linkedin"></a>
+
+                                </span>
+                            </div>
+
+                            <div class="col col-md-12">
+                                This is the awesome fan blog of the Liverpool FC
+                            </div>
+                        </div>
+                        <div class="row footer-row-3">
+                            <div class=" col-md-6 order-2 order-md-1 float-left">
+                                &copy; Copyright 2023 - Liverpool Fan Club - All Rights Reserved.
+                            </div>
+                            <div class="col-md-6 order-1 order-md-1 float-right">
+                                Sitemap - Privacy Policy - Terms & Conditions - About
+                            </div>
+                        </div>
+                    </div>
+
+                </footer>
+                <!--FOOTER ENDS HERE-->
+            </div>
+            <!--content wrapper-->
+            <!--########################FOOTER ENDS HERE####################-->
+        </div>
+    </div>
+    <!--site container-->
+
+
+
+
+</body>
+
+</html>
---
+++ task2/header.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <link href="https://fonts.cdnfonts.com/css/roboto-condensed" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <title>LFC Fan Club</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body class="wrapper">
+    <div class="container wd-100">
+
+        <!--####################### HEADER Begins ##################-->
+        <!--Banner begins Here-->
+        <div class="row topbar">
+        </div>
+        <!--Banner Ends Here-->
+
+        <div class="row header">
+            <p></p>
+        </div>
+
+        <nav class="navbar navbar-expand-xl  navbar-dark ms-auto pr-3   ">
+
+            <a class="navbar-brand justify-content-start" href="index.html">
+                <!-- Liverpool logo in the navbar-->
+                <img class="navbar_logo" src="lfc_logo.png" alt="Liverpool FC logo">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav topnav ms-auto float-lg-end">
+                    <li class="nav-item active">
+                    </li>                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="..\ex1.php">Exercise 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Video</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Schedule</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            More
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Match day</a>
+                            <a class="dropdown-item" href="#trophies">Championship wall</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">History</a>
+                        </div>
+                    </li>
+
+                </ul>
+
+                <!--user login links-->
+                <div class="nav-item user-box ">
+
+                    <a class="nav-link user-link " href="#">
+                        <img src="user.svg" width="18" alt="Signup" class="nav-icon"> Join</a>
+
+
+                    <a class="nav-link user-link" href="#">Login</a>
+
+
+                </div>
+                <!--user-box login links end-->
+                <div class="nav-item justify-content-end sponser">
+                    <a href="https://www.sc.com/en/"><img src="sponser.svg" width="120" alt="sponser"></a>
+                </div>
+            </div>
+        </nav>
+        <!--#################### HEADER ENDS #########################-->
